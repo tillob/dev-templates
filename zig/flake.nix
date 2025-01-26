cab201bf5bd1ad59fb1a6c714b59d98f3b04924c
tillob <tilman.orlob@web.de>
2025-01-26 19:15:13 +0100

    update nix lates

--- zig/flake.nix
+++ zig/flake.nix
@@ -13,15 +13,18 @@
   }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    zig = inputs.zig-overlay.packages.x86_64-linux.default;
-    # zig = inputs.zig-overlay.packages.x86_64-linux.master;
+    # zig = inputs.zig-overlay.packages.x86_64-linux.default;
+    zig = inputs.zig-overlay.packages.x86_64-linux.master;
+    zls = inputs.zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
+            nativeBuildInputs = [ zig ];
+          });
   in
   {
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = [
-        pkgs.zls
+        zls
         zig
-     
+
       ];
     };
   };
